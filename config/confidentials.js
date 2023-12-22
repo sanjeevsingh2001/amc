5e870620077816e3833cc6ea26d324e8d156a1f5
sanjeevsingh2001 <153840889+sanjeevsingh2001@users.noreply.github.com>
2023-12-22 20:39:34 +0530

    Update confidentials.js

--- config/confidentials.js
+++ config/confidentials.js
@@ -2,7 +2,7 @@ const emai_password = "ryjkmvqagolvmrkd";
 const email_user = "manojkumarsinghy20@gmail.com";
 const secret_key = "yametekhudasaisanjeev";
 const data_password = "@#Sanjukumar123";
-const uri ="mongodb+srv://mac_admin:Sanjeevsingh2002@cluster0.jfkuagj.mongodb.net/?retryWrites=true&w=majority"
+const uri ="mongodb+srv://mac_admin:Sriyo79RzjMz3KzB@cluster0.jfkuagj.mongodb.net/?retryWrites=true&w=majority"
 
 module.exports = {
   emai_password,
